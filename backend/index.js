fa54bce2d7f50ff832cc2b7393ce08cf49f21713
Muskan-codebase <muskanmjvr@gmail.com>
2025-11-25 17:21:51 +0530

    implemented input sanitization

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,13 @@ const { rateLimiter } = require("./rate-limiter/rateLimiter")
 const googleOAuthRoute = require("./routes/google.OAuth.route")
 const cors = require("cors");
 const helmet = require("helmet");
+const sanitizeHtml = require('sanitize-html');
 require("./config")
 
 app.use(cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
 }))
 
 //built-in middleware to convert incoming request data to json()
@@ -21,20 +22,32 @@ app.use(express.json())
 // Protects against XSS, clickjacking, MIME type sniffing, and other attacks
 app.use(helmet())
 
+// Middleware to sanitize all string inputs in request body
+app.use((req, res, next) => {
+  if (req.body) {
+    for (const key in req.body) {
+      if (typeof req.body[key] === 'string') {
+        req.body[key] = sanitizeHtml(req.body[key]); // sanitize input
+      }
+    }
+  }
+  next();
+});
+
 //middleware to block API request after certain attempts within a specified time frame (15 mins)
 app.use(rateLimiter)
 
 const PORT = process.env.PORT || 9000;
 
 app.get("/", (_, res) => {
-    res.json("Hello World!🌐");
+  res.json("Hello World!🌐");
 })
 
 app.use("/api", userRoute);
 app.use("/api/auth", googleOAuthRoute)
 
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT:${PORT}`)
+  console.log(`Server is running on PORT:${PORT}`)
 })
 
 DBConnection();
