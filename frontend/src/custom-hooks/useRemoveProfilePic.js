6f81db798c9fa3c44ec604cfb2ce6423e2289f87
Muskan-codebase <muskanmjvr@gmail.com>
2025-11-26 13:31:20 +0530

    integrated remove profile pic custom hook

---
+++ frontend/src/custom-hooks/useRemoveProfilePic.js
@@ -0,0 +1,38 @@
+import toast, { Toaster } from "react-hot-toast";
+import axios from "axios";
+
+function useRemoveProfilePic() {
+
+    const removeProfilePic = async function () {
+
+        const token = localStorage.getItem("Token");
+
+        try {
+
+            const response = await axios.delete("http://localhost:3000/api/removeProfilePic", {
+                headers: {
+                    "Authorization": `Bearer ${token}`,
+                    "Content-Type": "Application/json"
+                }
+            })
+
+            console.log(response.data);
+            toast.success(response.data.message);
+            window.location.reload();
+
+
+        } catch (error) {
+
+            if (error.response.data && error.response.data.message) {
+                toast.error(error.response.data.message)
+            } else {
+                toast.error("Some Error occured during Signup Process. Try again ....")
+                console.log("Error", error);
+            }
+        }
+    }
+
+    return { removeProfilePic };
+}
+
+export default useRemoveProfilePic;
