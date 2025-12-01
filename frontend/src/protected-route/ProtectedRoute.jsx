aed82c26ca9a8f96f9d1a5f110d20cd7a4e0c7d7
Muskan-codebase <muskanmjvr@gmail.com>
2025-12-01 17:17:37 +0530

    added protected route

--- frontend/src/protected-route/ProtectedRoute.jsx
+++ frontend/src/protected-route/ProtectedRoute.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Outlet, Navigate } from 'react-router-dom';
+import { AuthContext } from '../context-api/AuthContext';
+import { useContext } from 'react';
 
 function ProtectedRoute() {
 
-    const userToken = localStorage.getItem("Token");
+    const { isAuthenticated, loading } = useContext(AuthContext);
 
-    return (userToken ? <Outlet></Outlet> : <Navigate to="/"></Navigate>)
+    if (loading) {
+        return <span className="loading loading-spinner loading-xl"></span>
+    }
+
+    return (isAuthenticated ? <Outlet></Outlet> : <Navigate to="/"></Navigate>)
 }
 
 export default ProtectedRoute
